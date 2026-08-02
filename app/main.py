7e1a7bce228e6cb305b51ef1b0d7190c9b5e2349
Siddharth Pandey <pasiddharth.19@gmail.com>
2026-08-02 17:05:07 +0530

    chore: configure Alembic migrations

--- app/main.py
+++ app/main.py
@@ -9,5 +9,3 @@ app.include_router(user.router)
 @app.get("/")
 def home():
     return {"message":"Backend is running."}
-
-
--- app/models/user.py
+++ app/models/user.py
@@ -9,6 +9,8 @@ using SQLAlchemy.
 from sqlalchemy import Column, Integer, String
 from app.db import Base
 
+# defining class & also when executed, SQLAlechemy's ORM machinery register this table information in Base, so now Base.metadata contains the table structure
+# And this is what Alembic will also read.
 class User(Base):
     __tablename__= "users"
 
